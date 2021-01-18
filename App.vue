f105b9a7d3a6cbe10a925d38ddbdb53bdd407b82
mokerying <2320694976@qq.com>
2021-01-18 13:06:05 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,41 @@
+
+  
+<script>
+	
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	@import './common/uni.css'; 
+	/*每个页面公共css */
+	@font-face {
+		font-family: Arial, sans-serif;
+		font-size: 16upx;
+	}
+	.clearfix:after {
+	  visibility: hidden;
+	  display: block;
+	  font-size: 0;
+	  content: " ";
+	  clear: both;
+	  height: 0;
+	}
+	.fs14{font-size: .88rem;}
+	.fr{float: right;}
+	.gray{color: #9B9B9B;}
+	.red{color: #F5222D;}
+	.orange{color: #FF9800;}
+	.green{color: #52C41A;}
+	.blue{color: #1890FF;}
+	.line-height20{line-height: 20upx;}
+</style>
---
+++ main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+import uniPop from './components/uni-popup/uni-popup.vue'
+// @import './common/uni.js';
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+Vue.component('uni-pop', uniPop) 
+const app = new Vue({
+    ...App
+})
+app.$mount()
